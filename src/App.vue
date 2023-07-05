12391a9e940ce64d7b1b7c8bbb001632fb9a12f8
jasonw <111627243+Jasonw372@users.noreply.github.com>
2023-07-05 20:36:51 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="text-3xl font-bold m-2">Hello world!</h1>
+
+      <nav
+        class="text-2xl m-2 bg-amber-100 w-3/12 flex justify-around align-middle dark:bg-slate-800 space-x-2"
+      >
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="m-2">
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div class="flex space-x-4">
+    <span @click.stop="toggleDark()">暗黑模式</span>
+
+    <el-switch size="small" v-model="isDark" />
+  </div>
+</template>
